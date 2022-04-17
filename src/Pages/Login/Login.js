b3d1ecaa9368112420e2b9bf14b2bbad390e9e5d
akram56789 <akramhossain56789@gmail>
2022-04-17 12:47:27 +0600

    banner-done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,1 +1,13 @@
+ import React from 'react';
+import { Link } from 'react-router-dom';
  
+ const Login = () => {
+     return (
+         <div>
+             <h1>this is Login</h1>
+             <Link to='/register'><button>go register</button></Link>
+         </div>
+     );
+ };
+ 
+ export default Login;
